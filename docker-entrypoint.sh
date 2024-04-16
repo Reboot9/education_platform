171f2a588a8de7c833f83be55086b7f1c2d5defc
Reboot <sergejskorikjr@gmail.com>
2024-04-16 18:41:42 +0300

    update dockerfiles

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+set -e
 
 # wait for Postgres
 ./wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT -t 60
 
 # run migrations, collect media, start the server
-python manage.py migrate
+until python3 manage.py migrate
+do
+  echo "Waiting for the database to be ready..."
+  sleep 2
+done
+
+echo "Database is ready."
 
 python manage.py collectstatic --noinput
 
